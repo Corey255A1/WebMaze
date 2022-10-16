c8911b87aa74ce4f4b979b1c7a9fdd8312f9deb2
Corey <corey255a1@gmail.com>
2022-10-16 17:47:46 -0400

    Adding Collisions

--- ts/Maze3D.ts
+++ ts/Maze3D.ts
@@ -6,15 +6,15 @@ export class Maze3D{
     private _scene:Scene;
     private _cell_size:number;
     private _wall_material:StandardMaterial;
-    constructor(scene:Scene, maze:Maze){
+    constructor(scene:Scene, maze:Maze, size:number){
         this._maze = maze;
         this._scene = scene;
-        this._cell_size = 1;
+        this._cell_size = size;
         this._wall_material = new StandardMaterial("wall_mat", this._scene);
         this._wall_material.diffuseColor = new Color3(0.5,0,0);
     }
 
-    CreateCell(cell:MazeCell):Mesh|null{
+    CreateCell(cell:MazeCell){//:Mesh|null{
         if(!cell.Connected){ return null; }
         const x = this._cell_size*cell.Position.X;
         const y = this._cell_size*cell.Position.Y;
@@ -22,11 +22,13 @@ export class Maze3D{
         floor.material = this._wall_material;
         floor.position.set(x,0,y);
         floor.rotate(Axis.X, Math.PI/2);
+        floor.checkCollisions = true;
         let walls:Array<Mesh> = [];
         if(cell.Front == MazeWall){
             const wall = MeshBuilder.CreatePlane("wall",{size:this._cell_size});
             wall.material = this._wall_material;
             wall.position.set(x,this._cell_size/2,y+this._cell_size/2);
+            wall.checkCollisions = true;
             //wall.rotate(Axis.X, Math.PI/2);
             walls.push(wall);
         }
@@ -34,6 +36,7 @@ export class Maze3D{
             const wall = MeshBuilder.CreatePlane("wall",{size:this._cell_size});
             wall.material = this._wall_material;
             wall.position.set(x+this._cell_size/2,this._cell_size/2,y);
+            wall.checkCollisions = true;
             wall.rotate(Axis.Y, Math.PI/2);
             walls.push(wall);
         }
@@ -42,6 +45,7 @@ export class Maze3D{
             const wall = MeshBuilder.CreatePlane("wall",{size:this._cell_size});
             wall.material = this._wall_material;
             wall.position.set(x,this._cell_size/2,y-this._cell_size/2);
+            wall.checkCollisions = true;
             wall.rotate(Axis.Y, Math.PI);
             walls.push(wall);
         }
@@ -50,11 +54,12 @@ export class Maze3D{
             const wall = MeshBuilder.CreatePlane("wall",{size:this._cell_size});
             wall.material = this._wall_material;
             wall.position.set(x-this._cell_size/2,this._cell_size/2,y);
+            wall.checkCollisions = true;
             wall.rotate(Axis.Y, -Math.PI/2);
             walls.push(wall);
         }
 
-        return Mesh.MergeMeshes(walls);
+        //return Mesh.MergeMeshes(walls);
     }
 
     CreateMaze():Mesh|null{
@@ -62,12 +67,15 @@ export class Maze3D{
         let itr_ptr = iterator.next();
         let cells:Array<Mesh> = [];
         while(itr_ptr.done == false){
-            const cell:Mesh|null = this.CreateCell(itr_ptr.value);
-            if(cell != null){
-                cells.push(cell);
-            }
+            //const cell:Mesh|null = this.CreateCell(itr_ptr.value);
+            this.CreateCell(itr_ptr.value);
+            // if(cell != null){
+            //     cell.checkCollisions = true;
+            //     cells.push(cell);
+            // }
             itr_ptr = iterator.next();
         }
-        return Mesh.MergeMeshes(cells);
+        return null;
+        //return Mesh.MergeMeshes(cells);
     }
 }
--- ts/Maze3DMain.ts
+++ ts/Maze3DMain.ts
@@ -1,13 +1,16 @@
-import { ArcRotateCamera, Engine, HemisphericLight, MeshBuilder, Scene, Vector3 } from "babylonjs";
+import { ArcRotateCamera, Engine, HemisphericLight, Mesh, MeshBuilder, RuntimeError, Scene, UniversalCamera, Vector3 } from "babylonjs";
 import { Maze } from "./Maze";
 import { Maze3D } from "./Maze3D";
 
 export class Maze3DMain{
     private _maze:Maze | null;
     private _maze_renderer:Maze3D | null;
+    private _maze_mesh:Mesh|null;
     private _scene:Scene;
     private _engine:Engine;
     private _canvas:HTMLCanvasElement;
+    private _camera:UniversalCamera;
+
     constructor(canvas_id:string){
         this._canvas = document.getElementById(canvas_id) as HTMLCanvasElement;
         if(this._canvas == null){ throw "Could not find canvas"; }
@@ -15,24 +18,32 @@ export class Maze3DMain{
 
         this._engine = new Engine(this._canvas, true);
         this._scene = new Scene(this._engine);
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
-        camera.attachControl(this._canvas, true);
+        //var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
+        //camera.attachControl(this._canvas, true);
+        this._camera = new UniversalCamera("UniversalCamera", new Vector3(1, 1, 1), this._scene); 
+
+        // Attach the camera to the canvas
+        this._camera.speed = 0.5;
+        this._camera.applyGravity = true;
+        this._camera.ellipsoid = new Vector3(1.2, 1, 1.2);
+        this._camera.position.y = 5;
+        this._camera.position.x = 5;
+        this._camera.position.z = 5;
+        this._camera.checkCollisions = true;
+        this._camera.attachControl(this._canvas, true);
+
+
         //var sanitycheck = MeshBuilder.CreateBox('box1',{size:2});
         var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this._scene);
-        this._maze = null;
-        this._maze_renderer = null;
+        this._maze = Maze.Generate(0,0,20,20);
+        this._maze_renderer = new Maze3D(this._scene, this._maze, 10);
+        this._maze_mesh = this._maze_renderer.CreateMaze();
+        //if(this._maze_mesh == null){ throw "Error Creating Maze"; }
+        //this._maze_mesh.checkCollisions = true;
 
         // run the main render loop
         this._engine.runRenderLoop(() => {
             this._scene.render();
         });
-    }
-
-    public Generate(size:number):Maze{
-        this._maze = Maze.Generate(0,0,50,50);
-        this._maze_renderer = new Maze3D(this._scene, this._maze);
-        this._maze_renderer.CreateMaze();
-        return this._maze;
-
     }
 }
