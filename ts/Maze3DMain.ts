8ef236764aa09412436149a80eddfd1d27f0d368
Corey <corey255a1@gmail.com>
2022-10-17 20:45:46 -0400

    Trying to figure out collisions Keyboard Ball movement

--- ts/Maze3DMain.ts
+++ ts/Maze3DMain.ts
@@ -1,6 +1,7 @@
 import { ArcRotateCamera, Engine, HemisphericLight, Mesh, MeshBuilder, RuntimeError, Scene, UniversalCamera, Vector3 } from "babylonjs";
 import { Maze } from "./Maze";
 import { Maze3D } from "./Maze3D";
+import { Maze3DPlayer } from "./Maze3DPlayer";
 
 export class Maze3DMain{
     private _maze:Maze | null;
@@ -9,8 +10,9 @@ export class Maze3DMain{
     private _scene:Scene;
     private _engine:Engine;
     private _canvas:HTMLCanvasElement;
-    private _camera:UniversalCamera;
-
+    //private _camera:UniversalCamera;
+    private _camera:ArcRotateCamera;
+    private _player:Maze3DPlayer;
     constructor(canvas_id:string){
         this._canvas = document.getElementById(canvas_id) as HTMLCanvasElement;
         if(this._canvas == null){ throw "Could not find canvas"; }
@@ -18,14 +20,18 @@ export class Maze3DMain{
 
         this._engine = new Engine(this._canvas, true);
         this._scene = new Scene(this._engine);
-        //var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
-        //camera.attachControl(this._canvas, true);
-        this._camera = new UniversalCamera("UniversalCamera", new Vector3(0, 10, 0), this._scene); 
+        this._player = new Maze3DPlayer();
+        this._camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 10, this._player.Mesh.position, this._scene);
+        this._camera.attachControl(this._canvas, true);
+        //this._camera = new UniversalCamera("UniversalCamera", new Vector3(0, 10, 0), this._scene);
 
+        
+        this._scene.onKeyboardObservable.add(this._player.ProcessKeyboard.bind(this._player))
+        this._scene.onBeforeRenderObservable.add(this._player.Update.bind(this._player));
         // Attach the camera to the canvas
         this._camera.speed = 0.5;
-        this._camera.applyGravity = true;
-        this._camera.ellipsoid = new Vector3(1.2, 1, 1.2);
+        //this._camera.applyGravity = true;
+        //this._camera.ellipsoid = new Vector3(1.2, 1, 1.2);
         // this._camera.position.y = 5;
         // this._camera.position.x = 5;
         // this._camera.position.z = 5;
