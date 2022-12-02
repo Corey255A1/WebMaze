0ed54cd2eeccb5003b92efe83cb9ba1c6ef1b7cb
Corey <Corey255A1@hotmail.com>
2022-12-01 23:28:44 -0500

    Working on distance sensors for mazing solving robot simulator

---
+++ ts/Sensor.ts
@@ -0,0 +1,32 @@
+import { AbstractMesh, Mesh, PickingInfo, Quaternion, Ray, RayHelper, Scene, Vector3 } from "babylonjs";
+
+export class Sensor{
+    private _ray:Ray;
+    private _mesh:Mesh;
+    private _offset:Vector3;
+    private _relative_direction:Vector3;
+    private _last_picked:PickingInfo | null;
+    constructor(mesh:Mesh, offset:Vector3, relative_direction:Vector3, max_range:number){
+        this._mesh = mesh;
+        this._offset = offset;
+        this._relative_direction = relative_direction;
+        this._last_picked = null;
+        this._ray = new Ray(mesh.position.add(offset), relative_direction, max_range);
+        
+        const ray_helper  = new RayHelper(this._ray);        
+        ray_helper.show(this._mesh.getScene());
+    }
+    public get Distance():number{
+        if(this._last_picked!=null){ return this._last_picked.distance; }
+        else return this._ray.length;
+    }
+    private FilterMesh(mesh:AbstractMesh):boolean{
+        return this._mesh != mesh;
+    }
+    public Update(scene:Scene){
+        this._mesh.computeWorldMatrix(true);
+        this._ray.direction = this._relative_direction.applyRotationQuaternion(Quaternion.RotationQuaternionFromAxis(this._mesh.right, this._mesh.up, this._mesh.forward));
+        this._ray.origin = Vector3.TransformCoordinates(this._offset, this._mesh.getWorldMatrix());
+        this._last_picked = scene.pickWithRay(this._ray,this.FilterMesh.bind(this));
+    }
+}
